e19cdebe918b6c483f34214fc1db2197c4ae4e1f
Kaighin McColl <kaighin@gmail.com>
2015-07-28 15:31:12 -0400

    Added code (ETC.m) and readme

---
+++ ETC.m
@@ -0,0 +1,79 @@
+function [RMSE_ETC, rho_ETC] = ETC(y)
+% ETC.m
+%
+% Extended Triple Collocation (ETC) is a technique for estimating the
+% root-mean-squared errors (RMSE) and correlation coefficients (rho) of three
+% measurement systems (e.g., satellite, in-situ and model-based products)
+% with respect to the unknown true value of the variable being measured 
+% (e.g., soil moisture, wind speed).
+%
+% INPUTS
+% y: an N x 3 matrix of temporally- and spatially-collocated observations 
+% from the three measurement systems, where N is the sample size. This 
+% must be the same for each measurement system. All NaNs must be removed.
+%
+% OUTPUTS
+% RMSE_ETC: a 3 x 1 vector of RMSEs of each of the measurement systems with
+% respect to the uknown true value of the variable being measured. The ith
+% row corresponds to the measurement system with observations in the ith
+% column of y.
+% rho_ETC: a 3 x 1 vector of correlation coefficients.
+% 
+% REFERENCE
+% For more details on ETC, see:
+%
+% McColl, K.A., J. Vogelzang, A.G. Konings, D. Entekhabi, M. Piles, A.
+% Stoffelen (2014). Extended Triple Collocation: Estimating errors and 
+% correlation coefficients with respect to an unknown target. Geophysical 
+% Research Letters 41:6229-6236
+%
+% If you use ETC, please cite this paper where appropriate.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Author: Kaighin McColl
+% Date: September 14, 2014
+
+    % Catch errors in inputs
+    if size(y,2) ~= 3
+        error('Error: Input data y must be an N x 3 matrix');
+    end
+    
+    if any(isnan(y(:)))
+        error('Error: Input data y must not contain NaNs');
+    end
+    
+    if length(unique(y(:,1))) == 1 || length(unique(y(:,2))) == 1 || length(unique(y(:,3))) == 1 
+        error('Error: the sample variance of each of the columns of y must be non-zero. Increase your sample size or reconsider using ETC.');
+    end
+    
+    % Estimate covariance matrix of the three measurement systems
+    Q_hat = cov(y);
+
+    rho_ETC = [sqrt(Q_hat(1,2)*Q_hat(1,3)/Q_hat(1,1)/Q_hat(2,3)); ...
+        sign(Q_hat(1,3)*Q_hat(2,3))*sqrt(Q_hat(1,2)*Q_hat(2,3)/Q_hat(2,2)/Q_hat(1,3)); ...
+        sign(Q_hat(1,2)*Q_hat(2,3))*sqrt(Q_hat(1,3)*Q_hat(2,3)/Q_hat(3,3)/Q_hat(1,2))];
+
+    MSE_ETC = [(Q_hat(1,1) - Q_hat(1,2)*Q_hat(1,3)/Q_hat(2,3)); ...
+        (Q_hat(2,2) - Q_hat(1,2)*Q_hat(2,3)/Q_hat(1,3)); ...
+        (Q_hat(3,3) - Q_hat(1,3)*Q_hat(2,3)/Q_hat(1,2))];
+
+    RMSE_ETC = sqrt(MSE_ETC);
+    
+    % If MSEs are negative, display a warning.
+    if any(~isreal(RMSE_ETC))
+        warning('Warning: at least one calculated RMSE is a complex number. This can happen if the sample size is too small, or if one of the assumptions of ETC is violated.');
+    end
+    % If correlation coefficients are complex, display a warning. 
+    if any(~isreal(rho_ETC))
+        warning('Warning: at least one calculated correlation coefficient is a complex number. This can happen if the sample size is too small, or if one of the assumptions of ETC is violated.');
+    end
+    
+end
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+Extended Triple Collocation (ETC)
+=================================
+
+Extended Triple Collocation (ETC) is a technique for estimating the root-mean-squared-errors (RMSE) and correlation coefficients (rho) of three measurement systems (e.g., satellite, in-situ and model-based products) with respect to the unknown true value of the variable being measured (e.g., soil moisture, wind speed). See McColl et al. (2014) for more details (full reference below).
+
+ETC.m is a Matlab function for implementing ETC.
+
+## FAQ
+### Why am I getting complex values for the estimated RMSEs and correlation coefficients?
+This can happen if your sample size is too small, causing the estimated covariance matrix Q_hat to be too noisy. It can also occur if one of the assumptions of ETC is violated (e.g., linear relationship between the measurement and true value, uncorrelated errors between measurements and with the truth).
+
+
+## Reference
+If you publish work that uses this code, please cite:
+>McColl, K.A., J. Vogelzang, A.G. Konings, D. Entekhabi, M. Piles, A. Stoffelen (2014). Extended Triple Collocation: Estimating errors and correlation coefficients with respect to an unknown target. Geophysical Research Letters 41:6229-6236
+
+## Disclaimer
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
